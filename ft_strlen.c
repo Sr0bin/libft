2a33673b894dfc7788c057884d3ea0bb9e6d3a5f
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2025-01-13 19:40:32 +0100

    Ajout de putnbr base

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 14:47:45 by rorollin          #+#    #+#             */
-/*   Updated: 2024/11/15 16:48:44 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:10:05 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	lenght;
 
 	lenght = 0;
+	if (s == NULL)
+		return (0);
 	while (s[lenght] != '\0')
 		lenght++;
 	return (lenght);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:57:22 by rorollin          #+#    #+#             */
-/*   Updated: 2024/11/29 02:15:00 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/01/13 19:25:36 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,14 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+size_t	ft_issign(char c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+size_t	ft_valid_base(char *base);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *nptr);
+void	ft_putnbr_base(long long int n, char *base);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
@@ -45,10 +48,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
+size_t	ft_putchar_fd(char c, int fd);
+size_t	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
 typedef struct s_list
 {
 	void			*content;
