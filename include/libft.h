6e084b0f5412446572bebab1f8904d7c85be33ab
Robin Rollin <rorollin@z4r9p1.42lyon.fr>
2025-02-12 17:55:59 +0100

    Changed possible problem in order in makefile + added global libft header

---
+++ include/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 17:38:39 by rorollin          #+#    #+#             */
+/*   Updated: 2025/02/12 17:40:39 by rorollin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include "char.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include "i_o.h"
+# include "list.h"
+# include "memory.h"
+# include "string.h"
+#endif
