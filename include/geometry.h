ec90173e02218f8999bf4865f45f40089d97eb8d
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2025-04-20 23:52:21 +0200

    Housekeeping

--- include/geometry.h
+++ include/geometry.h
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 14:28:19 by rorollin          #+#    #+#             */
-/*   Updated: 2025/04/16 17:29:04 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/04/20 23:51:38 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,18 @@ typedef struct s_matrix
 	size_t	col_size;
 }	t_matrix_struct;
 
-typedef t_matrix_struct*	t_matrix;
+typedef t_matrix_struct*		t_matrix;
 typedef const t_matrix_struct*	t_matrix_const;
 
-
 t_matrix	create_matrix(size_t row, size_t col);
-void	free_matrix(t_matrix matrix);
+void		*free_matrix(t_matrix matrix);
 t_matrix	matrix_copy(t_matrix_const matrix);
-float	matrix_get_coord(t_matrix matrix, size_t row, size_t col);
-void	matrix_set_coord(t_matrix matrix, size_t row, size_t col, float value);
-void	*matrix_set_identity(t_matrix matrix);
+float		matrix_get_coord(t_matrix matrix, size_t row, size_t col);
+void		matrix_set_coord(t_matrix mat, size_t row, size_t col, float value);
+void		*matrix_set_identity(t_matrix matrix);
 t_matrix	substract_matrix(t_matrix_const a, t_matrix_const b);
 t_matrix	add_matrix(t_matrix_const a, t_matrix_const b);
 t_matrix	scalar_matrix(t_matrix_const matrix, float scalar);
 t_matrix	multiply_matrix(t_matrix_const a, t_matrix_const b);
-void	print_matrix(t_matrix_const matrix);
+void		print_matrix(t_matrix_const matrix);
 #endif
